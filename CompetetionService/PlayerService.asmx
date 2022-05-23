67c7f46556fef0e2ebd210f0b52cc0907b13fa3a
Akhmadjon <mrjohnaccaount@gmail.com>
2022-05-23 10:49:32 +0500

    Just practices to create some services and use it. NOW IT IS time to create real services

---
+++ CompetetionService/PlayerService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="PlayerService.asmx.cs" Class="CompetetionService.PlayerService" %>
---
+++ CompetetionServiceClient/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using PlayerReference;
+using System;
+using System.Threading.Tasks;
+
+namespace CompetetionServiceClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            
+            HelloWorldRequest helloWorld = new HelloWorldRequest();
+            HelloWorldResponse helloWorldResponse = new HelloWorldResponse();
+            //PlayerReference.PlayerServiceSoapClient client = new PlayerReference.PlayerServiceSoapClient();
+           
+            var service = new PlayerReference.HelloWorldRequestBody();
+            var config = new PlayerServiceSoapClient.EndpointConfiguration();
+            var client = new PlayerReference.PlayerServiceSoapClient(config);
+            var result = await client.HelloWorldAsync();
+            Console.WriteLine(result.Body.HelloWorldResult);
+        }
+    }
+}
